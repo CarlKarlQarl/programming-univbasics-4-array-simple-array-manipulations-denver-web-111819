8d7e1679c9b685de13b76927133cddd1352f65d0
Karl Matthes <karlmatthes@gmail.com>
2019-11-02 19:56:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,21 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, item)
-  array.insert()
+  array.insert(4, item)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, item)
+  array.delete(item)
+end
+
+def using_delete_at(array, index)
+  array
 end
